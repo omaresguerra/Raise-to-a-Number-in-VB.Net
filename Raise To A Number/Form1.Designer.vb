24b4475ea690544f49408ddfc445145d849733e7
John Omar D. Esguerra <esguerrajohnomar@gmail.com>
2018-05-04 09:29:09 -0800

    First commit

---
+++ Raise To A Number/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRaise
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button
+        Me.btnClear = New System.Windows.Forms.Button
+        Me.btnEvaluate = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txtNum = New System.Windows.Forms.TextBox
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.lstAnswer = New System.Windows.Forms.ListBox
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.btnExit.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(258, 248)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(97, 28)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.btnClear.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(214, 142)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(141, 32)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnEvaluate
+        '
+        Me.btnEvaluate.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.btnEvaluate.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEvaluate.Location = New System.Drawing.Point(214, 104)
+        Me.btnEvaluate.Name = "btnEvaluate"
+        Me.btnEvaluate.Size = New System.Drawing.Size(141, 32)
+        Me.btnEvaluate.TabIndex = 3
+        Me.btnEvaluate.Text = "&Evaluate"
+        Me.btnEvaluate.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(10, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 18)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Enter Number:"
+        '
+        'txtNum
+        '
+        Me.txtNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtNum.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNum.Location = New System.Drawing.Point(125, 12)
+        Me.txtNum.Multiline = True
+        Me.txtNum.Name = "txtNum"
+        Me.txtNum.Size = New System.Drawing.Size(230, 29)
+        Me.txtNum.TabIndex = 7
+        Me.txtNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lstAnswer)
+        Me.GroupBox1.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 46)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(195, 249)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Result"
+        '
+        'lstAnswer
+        '
+        Me.lstAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lstAnswer.FormattingEnabled = True
+        Me.lstAnswer.ItemHeight = 18
+        Me.lstAnswer.Location = New System.Drawing.Point(6, 22)
+        Me.lstAnswer.Name = "lstAnswer"
+        Me.lstAnswer.Size = New System.Drawing.Size(183, 218)
+        Me.lstAnswer.TabIndex = 0
+        '
+        'frmRaise
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(367, 300)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtNum)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnEvaluate)
+        Me.Font = New System.Drawing.Font("Century Gothic", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmRaise"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Raise To A Number"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnEvaluate As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtNum As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents lstAnswer As System.Windows.Forms.ListBox
+
+End Class
---
+++ Raise To A Number/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class frmRaise
+
+
+    Private Sub btnExit_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        If MessageBox.Show("Do you really want to exit?", _
+                          "Close Form", MessageBoxButtons.YesNo, _
+                          MessageBoxIcon.Question) = _
+                          Windows.Forms.DialogResult.Yes Then
+            End
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        txtNum.Text = ""
+        txtNum.Focus()
+        lstAnswer.Items.Clear()
+
+    End Sub
+
+    Private Sub btnEvaluate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEvaluate.Click
+        Dim intCount As Integer
+        intCount = Val(txtNum.Text)
+
+        For count As Integer = 1 To intCount
+            lstAnswer.Items.Add("2 ^ " & count.ToString & " = " & 2 ^ count)
+        Next
+
+    End Sub
+End Class
